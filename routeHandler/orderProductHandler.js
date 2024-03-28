564a5397d99e0447cbe03fe9ab63bcb81fd8ea38
TahidulAlam <tahid.cse@gmail.com>
2024-03-29 00:03:59 +0600

    update

--- routeHandler/orderProductHandler.js
+++ routeHandler/orderProductHandler.js
@@ -15,7 +15,50 @@ router.get("/", async (req, res) => {
     });
   }
 });
+router.get("/state", async (req, res) => {
+  const { email, searchValue, role, currentPage, itemsPerPage } = req.query;
+  try {
+    let query = {};
+    if (role === "employee") {
+      if (!email) {
+        return res
+          .status(400)
+          .json({ message: "Missing email for employee role" });
+      }
+      query.email = email;
+    } else if (role === "admin") {
+      query = {};
+    } else {
+      return res.status(400).json({ message: "Invalid user" });
+    }
+    if (searchValue && searchValue.trim() !== " ") {
+      query.$or = [{ productCode: searchValue }];
+    }
 
+    const skip = currentPage * itemsPerPage;
+    console.log(skip);
+
+    const items = await OrderProduct.find(query)
+      .skip(skip)
+      .limit(itemsPerPage)
+      .sort({ deliveryDate: -1 });
+    if (!items || items.length === 0) {
+      return res.status(404).json({
+        message: "No items found for the given email and search term",
+      });
+    }
+    // console.log(items);
+
+    // Total number of blogs
+    const totalCount = await OrderProduct.countDocuments();
+    res.status(200).json({ items, totalCount });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error occurred while searching for items",
+    });
+  }
+});
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   const query = { _id: new Object(id) };
